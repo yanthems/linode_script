381684fa094cc2c1e4ede2285a152b4b2c3225ac
yanthems <37106280+yanthems@users.noreply.github.com>
2018-11-22 12:13:03 +0800

    Create install_golang.sh

---
+++ install_golang.sh
@@ -0,0 +1,22 @@
+
+# work in ~/
+
+wget https://dl.google.com/go/go1.11.2.linux-amd64.tar.gz
+
+tar -zxf go1.11.2.linux-amd64.tar.gz
+
+mv go /usr/share/
+
+mkdir go
+
+cat >> ~/.bashrc
+
+export GOROOT=/usr/share/go
+export PATH=$GOROOT/bin:$PATH
+export GOPATH=~/go
+
+<<EOF
+
+. ~/.bashrc
+
+go version
