de5567ee85ef1fad071dc429e964ae4fa89bd2ca
yanthems <37106280+yanthems@users.noreply.github.com>
2018-11-22 12:25:08 +0800

    Create install_cmake.sh

---
+++ install_cmake.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+wget https://github.com/Kitware/CMake/releases/download/v3.13.0/cmake-3.13.0-Linux-x86_64.tar.gz
+
+tar -zxf cmake-3.13.0-Linux-x86_64.tar.gz
+
+mv cmake-3.13.0-Linux-x86_64 cmake-3.13
+
+mv cmake-3.13 /usr/local/share/
+
+cat <<EOF >> ~/.bashrc
+
+export CMAKE_ROOT=/usr/local/share/cmake-3.13
+export PATH=$CMAKE_ROOT/bin:$PATH
+
+EOF
+
+. ~/.bashrc
+
+cmake --version
