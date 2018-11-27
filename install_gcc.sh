fb5f95e5d80b66477dced349ebe1d7f2bdbcd4ee
yanthems <37106280+yanthems@users.noreply.github.com>
2018-11-27 14:43:48 +0800

    Update install_gcc.sh

--- install_gcc.sh
+++ install_gcc.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-8.2.0/gcc-8.2.0.tar.gz
+#8.2.0
+#7.3.0
 
-tar -zxf gcc-8.2.0.tar.gz
+wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-7.3.0/gcc-7.3.0.tar.gz
 
-cd gcc-8.2.0.tar.gz
+tar -zxf gcc-7.3.0.tar.gz
+
+cd gcc-7.3.0.tar.gz
 
 #下载依赖
 ./contrib/download_prerequisites 
 
-mkdir build-gcc-8.2.0
+mkdir build-gcc-7.3.0
 
-cd build-gcc-8.2.0
+cd build-gcc-7.3.0
 
 ../configure --enable-checking=release --enable-languages=c,c++ --disable-multilib 
 
